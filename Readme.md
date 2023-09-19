3cb883d33aab41486f6894fc17b8227e1f1617c1
zacharakisvasileios <zacharakis.vasileios@gmail.com>
2023-09-19 21:25:35 +0200

    Updated Readme file

--- Readme.md
+++ Readme.md
@@ -2,3 +2,16 @@
 
 In order to run application use:
 `docker-compose up`
+
+# API endpoints
+
+| Method | URL                          | Description           |
+| ------ | ---------------------------- | --------------------- |
+| `POST` | `/feedDB`                    | Populate database.    |
+| `POST` | `/message/create`            | Create a new message. |
+| `POST` | `/message/update/28`         | Update message #28.   |
+| `POST` | `/message/get/all`           | Get all messages      |
+| `POST` | `/message/clear`             | Deletes all messages. |
+| `POST` | `/user/`                     |                       |
+| `POST` | `/user/get/messageExchange`  |                       |
+| `POST` | `/user/get/conversationList` |                       |
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,14 +4,14 @@ const router = require("express").Router();
 
 router.post("/feedDB", messageController.feedDB);
 router.post("/message/create", messageController.createMessage);
-router.put("/message/update/:messageId", messageController.updateMessage);
+router.post("/message/update/:messageId", messageController.updateMessage);
 router.post("/message/get/all", messageController.getMessages);
 router.post("/message/clear", messageController.deleteAllMessages);
 
 // CRUD Routes /users
-router.get("/user", userController.getUsers);
-router.get("/user/get/messageExchange", userController.getUserMessageExchange);
-router.get(
+router.post("/user", userController.getUsers);
+router.post("/user/get/messageExchange", userController.getUserMessageExchange);
+router.post(
   "/user/get/conversationList",
   userController.getUserConversationList,
 );
